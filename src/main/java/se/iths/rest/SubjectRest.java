62b32fd925bf8680cf3948f0e98ebbaf95325d49
Johan Åström <johan.astrom89@gmail.com>
2022-02-08 15:45:01 +0100

    Updated SubjectRest

--- src/main/java/se/iths/rest/SubjectRest.java
+++ src/main/java/se/iths/rest/SubjectRest.java
@@ -1,4 +1,39 @@
 package se.iths.rest;
 
+import se.iths.entity.Subject;
+import se.iths.exception.EntityNotFoundWebException;
+import se.iths.service.SubjectService;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@RequestScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("subjects")
 public class SubjectRest {
+
+    @Inject
+    private SubjectService subjectService;
+
+    @GET
+    @Path("{id}")
+    public Response getSubjectById(@PathParam("id") Long id) {
+        Subject subject = subjectService.getSubjectById(id);
+        if (subject == null) {
+            throw new EntityNotFoundWebException("No subject with the specified id found.", Response.Status.NO_CONTENT);
+        } else {
+            return Response.ok(subject).build();
+        }
+    }
+
+    @POST
+    public Response createSubject(Subject subject, @Context UriInfo uriInfo) {
+        Long subjectId = subjectService.createSubject(subject).getId();
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+        uriBuilder.path(String.valueOf(subjectId));
+        return Response.created(uriBuilder.build()).build();
+    }
 }
