fc3c333e6d9020b4883a52673527a43bd8bcca37
Johan Åström <johan.astrom89@gmail.com>
2022-02-08 15:36:44 +0100

    Updated SubjectService

--- src/main/java/se/iths/service/SubjectService.java
+++ src/main/java/se/iths/service/SubjectService.java
@@ -1,5 +1,7 @@
 package se.iths.service;
 
+import se.iths.entity.Subject;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -10,5 +12,12 @@ public class SubjectService {
     @PersistenceContext
     private EntityManager em;
 
+    public Subject createSubject(Subject subject){
+        em.persist(subject);
+        return subject;
+    }
 
+    public Subject getSubjectById(Long id){
+        return em.find(Subject.class, id);
+    }
 }
