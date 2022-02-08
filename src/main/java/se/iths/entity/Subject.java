f3c1e0a2dc94b7046b704d03748b893e89fbe392
Johan Åström <johan.astrom89@gmail.com>
2022-02-08 11:52:49 +0100

    Added teacher and subject entities

---
+++ src/main/java/se/iths/entity/Subject.java
@@ -0,0 +1,34 @@
+package se.iths.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Subject() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
